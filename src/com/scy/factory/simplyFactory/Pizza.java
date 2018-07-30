83ad59900841453ad0dcc747e29ec80872632b60
scy <835410808@qq.com>
2018-07-30 11:07:01 +0800

    简单工厂

--- src/com/scy/factory/simplyFactory/Pizza.java
+++ src/com/scy/factory/simplyFactory/Pizza.java
@@ -6,7 +6,7 @@ public abstract class Pizza {
 	String name;
 	String dough;
 	String sauce;
-	ArrayList toppings = new ArrayList<>();
+	ArrayList<String> toppings = new ArrayList<>();
 	
 	void prepare() {
 		System.out.println("Preparing "+name);
