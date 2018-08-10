b4fdcd5ed60e456956f878439ce55454cbf60d6b
scy <835410808@qq.com>
2018-08-10 17:25:24 +0800

    命令模式 part1

---
+++ src/com/scy/command/Command.java
@@ -0,0 +1,5 @@
+package com.scy.command;
+
+public interface Command {
+	public void execute();
+}
---
+++ src/com/scy/command/LightOnCommand.java
@@ -0,0 +1,15 @@
+package com.scy.command;
+
+public class LightOnCommand implements Command {
+	Light light;
+	
+	public LightOnCommand(Light light) {
+		this.light = light;
+	}
+
+	@Override
+	public void execute() {
+		light.on();
+	}
+	
+}
